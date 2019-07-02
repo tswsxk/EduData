b9ec4539ed519fb3fb4a49d5ab28027c9f4af5cb
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-07-02 11:16:31 +0800

    initialization

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# create by tongshiwei on 2019/6/25
+
+from setuptools import setup
+
+setup(
+    name='XKT',
+    version='0.0.1',
+    install_requires=[
+        'mxnet',
+        'tqdm',
+    ]  # And any other dependencies foo needs
+)
