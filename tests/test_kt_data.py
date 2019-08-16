11b266de3c5414f3ac8c4906fab33b2f5ef59186
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-08-16 20:24:43 +0800

    添加knowledge tracing任务数据转换接口pytest

---
+++ tests/test_kt_data.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# create by tongshiwei on 2019-8-14
+
+from EduData.kt_data import tl2json, json2tl
+
+
+def test_json2tl(tmp_path):
+    src = "../data/junyi/student_log_kt.json.small.test"
+    tl_tar = tmp_path / "student_log_kt.json.small.test.tl"
+    json_tar = tmp_path / "student_log_kt.json.small.test.json"
+    json2tl(src, tl_tar)
+    tl2json(tl_tar, json_tar)
