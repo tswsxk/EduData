9615ef3ed3312868298890ead33c5d074b757f03
tswsxk <tongsw@mail.ustc.edu.cn>
2019-08-23 21:41:05 +0800

    add new command

--- EduData/main.py
+++ EduData/main.py
@@ -4,6 +4,7 @@
 import fire
 
 from EduData.DataSet.download_data.download_data import get_data as download, list_resources as ls
+from EduData.Task.KnowledgeTracing.format import tl2json, json2tl
 
 if __name__ == '__main__':
     fire.Fire()
