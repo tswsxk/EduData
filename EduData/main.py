87b78be0f27342df49173fd2e19c6ede7f43d1fb
tswsxk <tongsw@mail.ustc.edu.cn>
2019-09-26 14:27:47 +0800

    新增数据分析工具

--- EduData/main.py
+++ EduData/main.py
@@ -5,6 +5,8 @@ import fire
 
 from EduData.DataSet.download_data.download_data import get_data as download, list_resources as ls
 from EduData.Task.KnowledgeTracing.format import tl2json, json2tl
+from EduData.Task.KnowledgeTracing.statistics import analysis_records as kt_stat
+from EduData.Tools.train_valid_test import train_valid_test, KFold as kfold
 
 if __name__ == '__main__':
     fire.Fire()
