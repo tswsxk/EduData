bc6ec7f7b503cdd24c6157aec882d6e640fed208
tswsxk <tongsw@mail.ustc.edu.cn>
2019-11-14 20:30:11 +0800

    重构download_data函数

--- EduData/DataSet/download_data/download_data.py
+++ EduData/DataSet/download_data/download_data.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # create by tongshiwei on 2019/7/2
 
-__all__ = ["url_dict", "get_data", "list_resources"]
+__all__ = ["URL_DICT", "get_data", "list_resources"]
 
 import os
 from urllib.request import urlretrieve
@@ -21,7 +21,7 @@ logger = config_logging(logger="downloader", console_log_level=LogLevel.INFO)
 
 prefix = 'http://base.ustc.edu.cn/data/'
 
-url_dict = {
+URL_DICT = {
     "assistment-2009-2010-skill":
         "http://base.ustc.edu.cn/data/ASSISTment/2009_skill_builder_data_corrected.zip",
     "assistment-2012-2013-non-skill":
@@ -36,16 +36,14 @@ url_dict = {
         "http://base.ustc.edu.cn/data/slepemapy.cz/",
     "synthetic":
         "http://base.ustc.edu.cn/data/synthetic/",
-    "toy":
-        "http://base.ustc.edu.cn/data/toy.csv",
-    "ktbs":
-        "http://base.ustc.edu.cn/data/ktbs/",
+    "ktbd":
+        "http://base.ustc.edu.cn/data/ktbd/",
 }
 
 
 def get_dataset_name():  # pragma: no cover
     urls = []
-    for i in url_dict.values():
+    for i in URL_DICT.values():
         if i not in urls:
             urls.append(i)
     url = prefix
@@ -65,51 +63,50 @@ def get_dataset_name():  # pragma: no cover
                 # 避免ASSISTment和junyi的重复
                 if temp not in ['http://base.ustc.edu.cn/data/ASSISTment/',
                                 'http://base.ustc.edu.cn/data/JunyiAcademy_Math_Practicing_Log/']:
-                    url_dict[h[:-1]] = temp
+                    URL_DICT[h[:-1]] = temp
 
 
-def download_data(url, data_dir, override):
-    urls = []
-    os.makedirs(data_dir, exist_ok=True)
-    if url.endswith('/'):  # pragma: no cover
-        # 以/结尾是文件夹，其余是文件
-        file_path = path_append(data_dir, url.split('/')[-2], to_str=True)
-        os.makedirs(file_path, exist_ok=True)
+def download_file(url, save_path, override):
+    logger.info(url + ' is saved as ' + save_path)
+    urlretrieve(url, save_path, reporthook=reporthook4urlretrieve)
+    print()
+    decompress(save_path)
+    if override:
+        os.remove(save_path)
+        logger.info(save_path + ' is deleted.')
+
+
+def download_data(url, data_dir, override, bloom_filter: set = None):
+    bloom_filter = set() if bloom_filter is None else bloom_filter
+
+    if url in bloom_filter:
+        return
+
+    if url.endswith("/"):  # 以/结尾是文件夹，其余是文件
+        _data_dir = path_append(data_dir, url.split('/')[-2], to_str=True)
+
         r = requests.get(url, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         soup = BeautifulSoup(r.text, "lxml")
         al = soup.find_all('a')
         for a in al:
-            # 获得文件名
+            # 获得链接名
             h = a.get('href')
             if h[0] != '.':
-                temp = url + h
-                # 避免重复
-                if temp not in urls:
-                    urls.append(temp)
-                    temp_path = path_append(file_path, h, to_str=True)
-                    logger.info(temp + ' is saved as ' + temp_path)
-                    # 下载
-                    urlretrieve(temp, temp_path, reporthook=reporthook4urlretrieve)
-                    print()
-                    # 解压
-                    decompress(temp_path)
-                    if override:
-                        os.remove(temp_path)
-                        logger.info(temp_path + ' is deleted.')
+                url_h = url + h
+                if url_h not in bloom_filter:
+                    download_data(url_h, _data_dir, override, bloom_filter)
+        bloom_filter.add(url)
+
     else:
-        file_path = path_append(data_dir, url.split('/')[-1], to_str=True)
-        logger.info(url + ' is saved as ' + file_path)
-        urlretrieve(url, file_path, reporthook=reporthook4urlretrieve)
-        print()
-        decompress(file_path)
-        if override:
-            os.remove(file_path)
-            logger.info(file_path + ' is deleted.')
+        os.makedirs(data_dir, exist_ok=True)
+        save_path = path_append(data_dir, url.split('/')[-1], to_str=True)
+        download_file(url, save_path, override)
+        bloom_filter.add(url)
 
 
-def get_data(dataset, data_dir=DEFAULT_DATADIR, override=False):
+def get_data(dataset, data_dir=DEFAULT_DATADIR, override=False, url_dict: dict = None):
     """
     Parameters
     ----------
@@ -119,10 +116,14 @@ def get_data(dataset, data_dir=DEFAULT_DATADIR, override=False):
         数据存储目录
     override: bool
         是否覆盖已存在的文件
+    url_dict:
+        链接名称与链接映射
+
     Returns
     -------
 
     """
+    url_dict = URL_DICT if not url_dict else url_dict
     try:
         return download_data(url_dict[dataset], data_dir, override)
     except FileExistsError:  # pragma: no cover
@@ -130,9 +131,4 @@ def get_data(dataset, data_dir=DEFAULT_DATADIR, override=False):
 
 
 def list_resources():
-    print("\n".join(url_dict))
-
-
-if __name__ == '__main__':
-    list_resources()
-    get_data("assistment-2009-2010-skill")
+    print("\n".join(URL_DICT))
