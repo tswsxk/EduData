3a870b2e226c0c59691ca4ed9fc161604f005c15
tswsxk <tongsw@mail.ustc.edu.cn>
2019-11-19 09:38:16 +0800

    增加新的命令

--- EduData/main.py
+++ EduData/main.py
@@ -7,6 +7,7 @@ from EduData.DataSet.download_data.download_data import get_data, list_resources
 from EduData.Task.KnowledgeTracing.format import tl2json, json2tl
 from EduData.Task.KnowledgeTracing.statistics import analysis_records
 from longling.ML.toolkit.dataset import train_valid_test, kfold
+from script.junyi_kt import extract_relations, build_json_sequence
 
 
 def cli():  # pragma: no cover
@@ -19,5 +20,13 @@ def cli():  # pragma: no cover
             "kt_stat": analysis_records,
             "train_valid_test": train_valid_test,
             "kfold": kfold,
+            "dataset": {
+                "junyi": {
+                    "kt": {
+                        "extract_relations": extract_relations,
+                        "build_json_sequence": build_json_sequence,
+                    }
+                }
+            }
         }
     )
