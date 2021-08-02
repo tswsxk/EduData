1a9bedf3e9257e05efdac1b3780d6c12b1909c9f
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-02 11:42:14 +0800

    [feat] update reporthook4urlretrieve

--- EduData/DataSet/download_data/utils.py
+++ EduData/DataSet/download_data/utils.py
@@ -48,28 +48,6 @@ def un_tar(file):  # pragma: no cover
     tar_file.extractall(path=uz_path)
 
 
-def reporthook4urlretrieve(blocknum, bs, size):
-    """
-
-    Parameters
-    ----------
-    blocknum:
-        已经下载的数据块
-    bs:
-        数据块的大小
-    size:
-        远程文件的大小
-
-    Returns
-    -------
-
-    """
-    per = 100.0 * (blocknum * bs) / size
-    if per > 100:
-        per = 100
-    flush_print('Downloading %.2f%% : %d | %d' % (per, blocknum * bs, size))
-
-
 def yes_no(ask):
     yes = set(['yes', 'y', 'ye', ''])
     no = set(['no', 'n'])
@@ -88,3 +66,84 @@ def timestamp2time(time_stamp):
     import time
     time_struct = time.localtime(time_stamp)
     return time.strftime('%a, %d %b %Y %H:%M:%S GMT', time_struct)
+
+
+def format_sizeof(num, suffix='', divisor=1000):
+    """
+    Code from tqdm
+
+    Formats a number (greater than unity) with SI Order of Magnitude
+    prefixes.
+
+    Parameters
+    ----------
+    num  : float
+        Number ( >= 1) to format.
+    suffix  : str, optional
+        Post-postfix [default: ''].
+    divisor  : float, optional
+        Divisor between prefixes [default: 1000].
+
+    Returns
+    -------
+    out  : str
+        Number with Order of Magnitude SI unit postfix.
+
+    Examples
+    --------
+    >>> format_sizeof(800000)
+    '800K'
+    >>> format_sizeof(40000000000000)
+    '40.0T'
+    >>> format_sizeof(1000000000000000000000000000)
+    '1000.0Y'
+    """
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 999.5:
+            if abs(num) < 99.95:
+                if abs(num) < 9.995:
+                    return '{0:1.2f}'.format(num) + unit + suffix
+                return '{0:2.1f}'.format(num) + unit + suffix
+            return '{0:3.0f}'.format(num) + unit + suffix
+        num /= divisor
+    return '{0:3.1f}Y'.format(num) + suffix
+
+
+def format_byte_sizeof(num, suffix='B'):
+    """
+    Code from longling
+
+    Examples
+    --------
+    >>> format_byte_sizeof(1024)
+    '1.00KB'
+    """
+    return format_sizeof(num, suffix, divisor=1024)
+
+
+def reporthook4urlretrieve(blocknum, bs, size):
+    """
+    Code from longling
+
+    Parameters
+    ----------
+    blocknum:
+        已经下载的数据块
+    bs:
+        数据块的大小
+    size:
+        远程文件的大小
+
+    Returns
+    -------
+
+    """
+    per = 100.0 * (blocknum * bs) / size
+    if per > 100:
+        per = 100
+    flush_print(
+        'Downloading %.2f%% : %s | %s' % (
+            per,
+            format_byte_sizeof(blocknum * bs),
+            format_byte_sizeof(size)
+        ))
